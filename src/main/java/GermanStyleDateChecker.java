75796c9a94611d833b6fc4ac4a6c6e697fc33771
Nenad TRIPIC <tripicnenad@hotmail.com>
2018-05-15 20:00:42 +0200

    Hausuebung abgegeben

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -1,8 +1,10 @@
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^((0)?[1-9]|[12]\\d|3[01])((\\.)|(-)| )((0)?[1-9]|1[0-2])((\\.)|(-)| )(([12][019])?\\d{2})$");
+        return p.matcher(dateString);
     }
 }
